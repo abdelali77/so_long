66c89747b6f2c6cc5c4cee0d2eefebd0f72da595
Abdelali <abde69436@gmail.com>
2024-05-16 20:13:35 +0100

    errors

--- errors.c
+++ errors.c
@@ -6,13 +6,13 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 18:59:55 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/05/14 20:09:36 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/05/16 16:10:26 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	print_err(void)
+void	print_err()
 {
 	ft_printf("Error\n");
 	exit(1);
@@ -39,6 +39,8 @@ void	check_len(char **map)
 	int		i;
 	size_t	len;
 
+	if (!map)
+		return ;
 	i = 0;
 	len = ft_strlen(map[i]);
 	while (map[i])
@@ -46,6 +48,7 @@ void	check_len(char **map)
 		if (ft_strlen(map[i]) != len)
 		{
 			ft_printf("Error, map is not rectangular\n");
+			free_arr(map);
 			exit(1);
 		}
 		i++;
@@ -57,13 +60,14 @@ bool	check_walls(char **map)
 	int	i;
 	int	j;
 
+	if (!map)
+		return (false);
 	i = 1;
 	j = 0;
 	while (map[0][j])
 	{
-		if (map[0][j] != '1')
+		if (map[0][j++] != '1')
 			return (false);
-		j++;
 	}
 	while (map[i])
 	{
@@ -74,9 +78,8 @@ bool	check_walls(char **map)
 	j = 0;
 	while (map[arr_len(map) - 1][j])
 	{
-		if (map[arr_len(map) - 1][j] != '1')
+		if (map[arr_len(map) - 1][j++] != '1')
 			return (false);
-		j++;
 	}
 	return (true);
 }
@@ -84,26 +87,29 @@ bool	check_walls(char **map)
 void	_check(char *map)
 {
 	int			i;
-	t_solong	*p;
+	t_solong	p;
 
-	p = malloc(sizeof(t_solong));
-	p->collectible = 0;
-	p->exit = 0;
-	p->player = 0;
+	if (!map)
+		return ;
+	p.collectible = 0;
+	p.exit = 0;
+	p.player = 0;
 	i = 0;
 	while (map[i] != '\0')
 	{
 		if (map[i] == 'C')
-			p->collectible++;
+			p.collectible++;
 		else if (map[i] == 'E')
-			p->exit++;
+			p.exit++;
 		else if (map[i] == 'P')
-			p->player++;
-		if (map[i] == '\n' && map[i + 1] == '\n')
-			print_err();
+			p.player++;
+		if (map[0] == '\n' || (map[i] == '\n' && map[i + 1] == '\n'))
+			ft_exit(map);
+		else if (map[i] != '1' && map[i] != '0' && map[i] != 'C'
+			&& map[i] != 'E' && map[i] != 'P' && map[i] != '\n')
+			ft_exit(map);
 		i++;
 	}
-	if (!(p->collectible >= 1 && p->exit == 1 && p->player == 1))
-		print_err();
-	free(p);
+	if (!(p.collectible >= 1 && p.exit == 1 && p.player == 1))
+		ft_exit(map);
 }
