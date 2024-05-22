dde3e6e65d72d3d93bb0baebbeaa4c4ca993fce2
Abdelali <abde69436@gmail.com>
2024-05-22 14:52:06 +0100

    directions

--- directions.c
+++ directions.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 20:52:04 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/05/21 14:27:19 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/05/22 13:43:17 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,69 +15,95 @@
 void	move_right(t_solong *game)
 {
 	int	y;
-	int x;
+	int	x;
 
 	y = game->marshall->instances->y / 64;
 	x = game->marshall->instances->x / 64;
 	if (game->map[y][x + 1] == '1')
-		return;
+		return ;
+	if (game->map[y][x] == 'C')
+	{
+		game->map[y][x] = '0';
+		game->collectible--;
+	}
+	mlx_image_to_window(game->mlx_ptr, game->space_load, x * 64, y * 64);
 	game->marshall->instances->x += 64;
-	check_collect(game);
-	ft_printf("RIGHT\n");
+	mlx_image_to_window(game->mlx_ptr, game->marshall,
+		game->marshall->instances->x,
+		game->marshall->instances->y);
+	mlx_image_to_window(game->mlx_ptr, game->ship_load,
+		game->ship_x, game->ship_y);
+	ft_printf("moves : %d\n", game->moves++);
 }
 
 void	move_left(t_solong *game)
 {
 	int	y;
-	int x;
+	int	x;
 
 	y = game->marshall->instances->y / 64;
 	x = game->marshall->instances->x / 64;
 	if (game->map[y][x - 1] == '1')
-		return;
+		return ;
+	if (game->map[y][x] == 'C')
+	{
+		game->map[y][x] = '0';
+		game->collectible--;
+	}
+	mlx_image_to_window(game->mlx_ptr, game->space_load, x * 64, y * 64);
 	game->marshall->instances->x -= 64;
-	check_collect(game);
-	ft_printf("LEFT\n");
+	mlx_image_to_window(game->mlx_ptr, game->marshall,
+		game->marshall->instances->x,
+		game->marshall->instances->y);
+	mlx_image_to_window(game->mlx_ptr, game->ship_load,
+		game->ship_x, game->ship_y);
+	ft_printf("moves : %d\n", game->moves++);
 }
 
 void	move_up(t_solong *game)
 {
 	int	y;
-	int x;
+	int	x;
 
 	y = game->marshall->instances->y / 64;
 	x = game->marshall->instances->x / 64;
 	if (game->map[y - 1][x] == '1')
-		return;
+		return ;
+	if (game->map[y][x] == 'C')
+	{
+		game->map[y][x] = '0';
+		game->collectible--;
+	}
+	mlx_image_to_window(game->mlx_ptr, game->space_load, x * 64, y * 64);
 	game->marshall->instances->y -= 64;
-	check_collect(game);
-	ft_printf("UP\n");
+	mlx_image_to_window(game->mlx_ptr, game->marshall,
+		game->marshall->instances->x,
+		game->marshall->instances->y);
+	mlx_image_to_window(game->mlx_ptr, game->ship_load,
+		game->ship_x, game->ship_y);
+	ft_printf("moves : %d\n", game->moves++);
 }
 
 void	move_down(t_solong *game)
 {
 	int	y;
-	int x;
+	int	x;
 
 	y = game->marshall->instances->y / 64;
 	x = game->marshall->instances->x / 64;
 	if (game->map[y + 1][x] == '1')
-		return;
-	game->marshall->instances->y += 64;
-	check_collect(game);
-	ft_printf("DOWN\n");
-}
-
-void	check_collect(t_solong *game)
-{
-	int	y;
-	int x;
-
-	y = game->marshall->instances->y / 64;
-	x = game->marshall->instances->x / 64;
+		return ;
 	if (game->map[y][x] == 'C')
 	{
-		mlx_image_to_window(game->mlx_ptr, game->texture->wall_load,
-					y * 64, x * 64);
+		game->map[y][x] = '0';
+		game->collectible--;
 	}
+	mlx_image_to_window(game->mlx_ptr, game->space_load, x * 64, y * 64);
+	game->marshall->instances->y += 64;
+	mlx_image_to_window(game->mlx_ptr, game->marshall,
+		game->marshall->instances->x,
+		game->marshall->instances->y);
+	mlx_image_to_window(game->mlx_ptr, game->ship_load,
+		game->ship_x, game->ship_y);
+	ft_printf("moves : %d\n", game->moves++);
 }
